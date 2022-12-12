65ce02b5fa9517d3cbbef53fd45c49f8573f4423
Ivan Esteban <ivan.esteban@guidesmiths.com>
2022-12-12 10:33:41 +0100

    added templates and days missing tbd

---
+++ day9/part1.js
@@ -0,0 +1,9 @@
+// Personal data set
+const inputCase = ``;
+
+// Solution
+const resolve = (input) => console.log('WRITE YOUR SOLUTION');
+
+const output = resolve(inputCase);
+
+console.log(`The solution is ${output}`);
