5acd5c74f5dd4969c01800a7fd72de77b29d995f
Ivan Esteban <ivan.esteban@guidesmiths.com>
2022-12-12 10:33:26 +0100

    added templates and days missing tbd

--- day4/template.js
+++ day4/template.js
@@ -1,15 +1,8 @@
 // Personal data set
 const inputCase = ``;
 
-// Provided test case
-const testCase = ``;
-
 // Solution
 const resolve = (input) => console.log('WRITE YOUR SOLUTION');
-
-const test = resolve(testCase);
-
-console.assert(test === 'PUT HERE TEST SOLUTION', 'Test case failed');
 
 const output = resolve(inputCase);
 
